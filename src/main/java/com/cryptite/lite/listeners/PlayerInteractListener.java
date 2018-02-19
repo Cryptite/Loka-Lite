c25b3c7d1031cb76a87513048b724328ea22df04
cryptite <cryptite@gmail.com>
2018-02-19 10:50:00 -0600

    No interacting with entities or armor stands

--- src/main/java/com/cryptite/lite/listeners/PlayerInteractListener.java
+++ src/main/java/com/cryptite/lite/listeners/PlayerInteractListener.java
@@ -3,6 +3,8 @@ package com.cryptite.lite.listeners;
 import com.cryptite.lite.LokaLite;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerArmorStandManipulateEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class PlayerInteractListener implements Listener {
@@ -15,6 +17,16 @@ public class PlayerInteractListener implements Listener {
 
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent e) {
+        e.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onArmorStandManipulate(PlayerArmorStandManipulateEvent e) {
+        e.setCancelled(true);
+    }
+
+    @EventHandler
+    private void onPlayerInteractEntity(PlayerInteractEntityEvent e) {
         e.setCancelled(true);
     }
 }
